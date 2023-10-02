16d518c289ebb4c780d05fff2a8d13a4512b416a
Yang Tian <tian76032@gmail.com>
2023-10-01 17:52:33 -0700

    usage

--- submit.sh
+++ submit.sh
@@ -5,5 +5,5 @@ git push -u origin main
 # compiled it into web version
 hugo --destination compiled_jojo_notes
 git add .
-git commit -m '$1 compiled'
+git commit -m "$1 compiled"
 git push -u origin main
